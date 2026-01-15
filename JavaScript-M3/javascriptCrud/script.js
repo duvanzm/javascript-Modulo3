df07308ec8ab4c883f658a0f6c671bbb42a5c082
duvanzm <dazm0998@gmail.com>
2026-01-15 18:59:12 -0500

    Add files via upload

--- JavaScript-M3/javascriptCrud/script.js
+++ JavaScript-M3/javascriptCrud/script.js
@@ -3,15 +3,66 @@ let nombre = document.getElementById("text-name");
 let lastname = document.getElementById("text-lastname");
 let email = document.getElementById("text-email");
 let listAdd = document.querySelector("#list");
+// Modal
+const modalSave = document.getElementById("modalSave");
+const mdalNombre = document.getElementById("modal-name");
+const mdalLastname = document.getElementById("modal-lastname");
+const mdalEmail = document.getElementById("modal-email");
+//---------------------------------
 let counter = 0;
-const listUsers = [];
+const listUsers = [
+  {
+    id: 100,
+    nombre: "duvan",
+    lastname: "zuluaga",
+    email: "dz@",
+  },
+  {
+    id: 99,
+    nombre: "duvanzm",
+    lastname: "zuluagamm",
+    email: "dz@",
+  },
+];
 
-function btninfo(info){
-    
-}
+// Funciones de los Botones
+
+listAdd.addEventListener("click", function (e) {
+  const myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+
+  // Elimina
+  if (e.target.classList.contains("pop")) {
+    e.target.closest("tr").remove();
+  }
+
+  // Update
+  if (e.target.classList.contains("update")) {
+    const dadButtonFull = e.target.closest("tr");
+    const idNum = Number(dadButtonFull.querySelector("th").innerText);
+
+    const user = listUsers.find((u) => u.id === idNum);
+    console.log(user, idNum);
+    mdalNombre.value = user.nombre;
+    mdalLastname.value = user.lastname;
+    mdalEmail.value = user.email;
+    myModal.show();
+  }
+
+  // Detail
+  if (e.target.classList.contains("info")) {
+    alert("info");
+  }
+});
+// Guardar cambios desde modal
+modalSave.addEventListener("click", function () {
+  const user = listUsers.find((u) => u.id === currentUserId);
+  user.nombre = mdalNombre.value;
+  user.lastname = mdalLastname.value;
+  user.email = mdalEmail.value;
+});
 
 button.addEventListener("click", function () {
-  if (nombre.value != "" && lastname.value != "") {
+  if (nombre.value != "" && lastname.value != "" && email.value != "") {
     nombre.classList.add("is-valid");
     nombre.classList.remove("is-invalid");
     lastname.classList.add("is-valid");
@@ -24,35 +75,36 @@ button.addEventListener("click", function () {
       icon: "success",
     });
     let newUser = {
-      id: 0,
+      id: "",
       nombre: "",
       lastname: "",
+      email: "",
     };
-
-    newUser.id += counter;
+    // this.parentElement.parentElement.remove();
+    newUser.id = counter;
     newUser.nombre = nombre.value;
     newUser.lastname = lastname.value;
+    newUser.email = email.value;
     sessionStorage;
     listUsers.push(newUser);
     nombre.value = "";
     lastname.value = "";
     email.value = "";
-    userList = ` 
+
+    listUsers.forEach(function () {
+      userList = `
     <tr>
     <th>${newUser.id}</th>
     <td>${newUser.nombre}</td>
     <td>${newUser.lastname}</td>
     <td>${newUser.email}</td>
-    <td><button class="btn btn-danger pop" onClick="this.parentElement.parentElement.remove()">Delete</button><button class="btn btn-warning update " onClick="">Update</button><button class="btn btn-info info" onClick="">Detail</button></td>
+    <td><button class="btn btn-danger pop">Delete</button><button class="btn btn-warning update">Update</button><button class="btn btn-info info">Detail</button></td>
     </tr>`;
-    counter += 1;
-    listAdd.innerHTML += userList;
-    // listAdd.addEventListener("click", function (e) {
-    //   if (e.target.classList.contains("pop")) {
-    //     e.target.closest("tr").remove();
-    //   }
-    // });
-    console.log(listUsers);
+
+      listAdd.innerHTML += userList;
+    });
+    counter++;
+    // console.log(listUsers);
   } else {
     nombre.classList.add("is-invalid");
     nombre.classList.remove("is-valid");
